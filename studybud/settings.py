23d8c78b891dca04815b6de951870ca045a7a893
sajeel12 <msajeelahmad2001@gmail.com>
2022-11-02 12:00:43 +0500

    commit_second

--- studybud/settings.py
+++ studybud/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = 'django-insecure-@gpr&@@m5ru9d9+%tlrwf(5ng8+=c8jq9mq^q1arpm@c3jp&y^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['127.0.0.1', '*']
 
 # Application definition
 
