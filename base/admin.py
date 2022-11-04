53825854461f431601000c6ce37e98493566d498
sajeel12 <msajeelahmad2001@gmail.com>
2022-11-04 08:40:18 +0500

    commit_second

--- base/admin.py
+++ base/admin.py
@@ -8,3 +8,7 @@ admin.site.register(User)
 admin.site.register(Room)
 admin.site.register(Message)
 admin.site.register(Topic)
+
+dmin.site.site_header = "__Masoom__ Admin"
+admin.site.site_title = "__Masoom__ Admin Portal"
+admin.site.index_title = "__Masoom__  Nizame-e-Alam"
